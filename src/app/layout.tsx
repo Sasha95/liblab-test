cf34218b5509fc19026f3a8dada385be829f2219
@sasha982025 <sasha982025@gmail.com>
2025-05-08 12:09:57 +0300

    chore(layout): update app title in metadata to 'The Lord of the Rings'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'liblab test app',
+  title: 'The Lord of the Rings',
   description: 'liblab test app for frontend interview',
 };
 
